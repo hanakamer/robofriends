8af6336f089b228fdb7e7e93dae88a6b142d31e3
Hanay Can <hanaycan@Hanays-MacBook-Pro.local>
2021-09-09 15:00:02 +0300

    react robofriends app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
-import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 
 
 class App extends Component{
@@ -25,24 +23,22 @@ class App extends Component{
 		
 	}
 	render() {
-		const filteredRobots =  this.state.robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		const { robots, searchfield } = this.state;
+		const filteredRobots =  robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
 		})
-		if(this.state.robots.length === 0) {
-			return <h1>Loading</h1>
-		} else {
-			return (
+		return !robots.length ?
+		<h1>Loading</h1> :
+		(
 		<div className='tc'>
 			<h1>Robo Friends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
 			<Scroll>
-				<ErrorBoundry>
 				<CardList robots={filteredRobots}/>
-				</ErrorBoundry>
 			</Scroll>
 		</div>
 		);
-		}
+		
 		
 	}
 }
